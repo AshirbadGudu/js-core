be1a9b523db575507bd7a1ba27cd8859e3171698
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-10-09 20:59:11 +0530

    getArrFromSnap &  formatCurrency utility functions added

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+// Convert an object to array
 const getArrFromObj = (object, arr = []) => {
   for (const id in object) arr.push({ id, ...object[id] });
   return arr;
 };
+// Convert Firebase snapshot to array
+const getArrFromSnap = (snap, key = "id", arr = []) => {
+  const snapVal = snap.val();
+  if (snap.exists())
+    for (const id in snapVal) arr.push({ ...snapVal[id], [key]: id });
+  return arr;
+};
+// Format Currency
+const formatCurrency = (amount, currency_code = "en-IN") =>
+  new Intl.NumberFormat(currency_code, {
+    style: "currency",
+    currency: "INR",
+  }).format(amount);
 
-module.exports = { getArrFromObj };
+module.exports = { getArrFromObj, getArrFromSnap, formatCurrency };
